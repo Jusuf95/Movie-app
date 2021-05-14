f86498a1864e2ecb2ef29541076a35454ab4b379
Jusuf95 <73205233+Jusuf95@users.noreply.github.com>
2021-05-14 02:45:34 +0200

    Add files via upload

---
+++ Movie-app/src/components/Search.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react"
+import {observer} from "mobx-react"
+import {Link} from "react-router-dom"
+import store from "../store/home"
+
+
+const nullw500 = require('../images/nullw500.png')
+
+const Search = observer(class extends Component {
+    render() {
+      const {searchResults, loaded} = store
+      const {changePage, scrollTop} = this.props
+      
+      return (
+          <section>
+              
+                {loaded ? <div className="movies-grid">
+                    {
+                searchResults.results.map(({id, 
+                poster_path, 
+                release_date, 
+                original_title, 
+                vote_average}) => (
+                    <div 
+                    className="movie-item infos-container" 
+                    key={id}>
+                        <Link to={`/movie/${id}`} onClick={scrollTop}>       
+                            <img src={poster_path ? 
+                            `https://image.tmdb.org/t/p/w500${poster_path}` :
+                                `${nullw500}`} 
+                                alt={`Movie Poster`}/>
+                            <div className="infos-box">
+                                <div className="infos-one">{release_date}</div>
+                                <div className="infos-two">{original_title}</div>
+                                <div className="infos-three">{vote_average}</div>
+                            </div>
+                        </Link>
+                    </div>))}
+                    </div> : 
+                    <div className="loading">Loading...</div>
+                }
+               
+          </section>
+      )
+    }
+})
+
+export default Search
---
+++ Movie-app/src/store/movie.js
@@ -0,0 +1,75 @@
+import { observable, action, decorate, runInAction } from "mobx"
+
+const html = document.querySelector('html')
+ 
+// Store for fetching the Movies Page
+class Movie {
+  
+  details = []
+  credits = []
+  loaded = false
+  recommendations = []
+
+  fetchAll(id) {
+    runInAction(() => {
+      this.loaded = false
+    })
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=82dee22856e0d0ac5f767ec6fb845efc&language=en-US`
+    )
+      .then(res => res.json())
+      .then(res => {
+        return (
+          
+          this.setDetails(res),
+          this.details ? html.style.background = `url(https://image.tmdb.org/t/p/w1280${this.details.backdrop_path}) 
+                center center / cover no-repeat fixed` : null
+        )
+    })
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/credits?api_key=82dee22856e0d0ac5f767ec6fb845efc`
+    )
+      .then(res => res.json())
+      .then(res => {
+        this.setCredits(res)
+      })
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=82dee22856e0d0ac5f767ec6fb845efc&language=en-US&page=1`
+    )
+    
+    .then(res => res.json())
+    .then(res => (
+      this.setRecommendations(res)
+    ))
+  }
+
+  setDetails(data) {
+    this.details = data
+  }
+
+  setCredits(data) {
+    this.credits = data
+  }
+
+  setRecommendations(data) {
+    this.recommendations = data
+    this.loaded = true
+  }
+}
+
+decorate(Movie, {
+  details: observable,
+  credits: observable,
+  loaded: observable,
+  recommendations: observable,
+  setDetails: action,
+  setCredits: action,
+  setRecommendations: action
+})
+
+let movieStore = new Movie()
+
+export default movieStore
